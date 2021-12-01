7c267fa8def92a671d8be984bd97e2cd1f59f792
Grant Lee <Grant_lee1@outlook.com>
2021-11-30 19:49:33 -0500

    Initial commit

---
+++ Calculations.java
@@ -0,0 +1,34 @@
+// This class will handle all of our 4 function calculations
+public class Calculations 
+{
+	double value;
+	
+	public double add(double a, double b)
+	{
+		double sum;
+		sum = a + b;
+		return sum;
+	}
+	
+	public double sub(double a, double b)
+	{
+		double difference;
+		difference = a - b;
+		return difference;
+	}
+	
+	public double multiply(double a, double b)
+	{
+		double product;
+		product = a * b;
+		return product;
+	}
+	
+	public double quotient(double a, double b)
+	{
+		double quotient;
+		quotient = a / b;
+		return quotient;
+	}
+	
+}
---
+++ Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator 
+{
+	public static void main(String[] args) 
+	{
+		Calculations x = new Calculations();
+		
+		double result = x.add(5, 9);
+		
+		System.out.println("Sum is: " + result);
+	}
+}
